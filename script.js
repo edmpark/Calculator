47efeb9495ed60fdfdc8bb880f9f2419cd821c01
Edmond P <edmondpark1@gmail.com>
2020-01-12 18:21:49 -0800

    has all basic operation function

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    
+    return a/b;
+}
+
+function operate(op,a,b){
+    if(op === '+'){
+        add(a,b);
+    }else if(op === '-'){
+        subtract(a,b);
+    }else if(op == '*'){
+        multiply(a,b);
+    }else if(op == '/'){
+        divide(a,b);
+    }
 }
